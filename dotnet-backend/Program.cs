03f399efacec3c9594050df9c09953a8f44af21e
Damian Amalraj <damian.amalraj@hotmail.com>
2024-01-18 16:21:01 +0100

    🚀 Update: Update Program.cs for production

--- dotnet-backend/Program.cs
+++ dotnet-backend/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-string connectionString = builder.Configuration.GetConnectionString("ProdConnection");
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(connectionString));
 
