4ee1fc1553154ff517254a8880946046cb2c90ae
Damian Amalraj <damian.amalraj@hotmail.com>
2024-01-18 16:15:08 +0100

    🚀 Update: Update appSettings for production

--- dotnet-backend/Program.cs
+++ dotnet-backend/Program.cs
@@ -42,11 +42,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
